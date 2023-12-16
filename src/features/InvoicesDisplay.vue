40b79c8d81cc85739c8e1c340055b8944fbaaf85
Nemanja <malesija.nemanja@gmail.com>
2023-12-16 01:28:53 +0100

    Play around with options api

--- src/features/InvoicesDisplay.vue
+++ src/features/InvoicesDisplay.vue
@@ -1,8 +1,8 @@
 <script setup lang="ts">
-import useInvoices from './useInvoices';
-import LoadingSpinner from '../ui/LoadingSpinner.vue';
-import SingleInvoice from './SingleInvoice.vue';
-import EmptyHomePage from '@/ui/EmptyHomePage.vue';
+import useInvoices from "./useInvoices";
+import LoadingSpinner from "../ui/LoadingSpinner.vue";
+import SingleInvoice from "./SingleInvoice.vue";
+import EmptyHomePage from "@/ui/EmptyHomePage.vue";
 
 const { isLoading, invoices } = useInvoices();
 </script>
@@ -13,8 +13,15 @@ const { isLoading, invoices } = useInvoices();
   <div v-if="invoices" class="col-start-2 grid bg-gray-50">
     <div>
       <EmptyHomePage v-if="invoices.length === 0" />
-      <div v-else class="border-[1px] border-gray-100/20 flex flex-col rounded-md bg-gray-0">
-        <SingleInvoice v-for="invoice in invoices" :key="invoice.id" :invoice="invoice" />
+      <div
+        v-else
+        class="border-[1px] border-gray-100/20 flex flex-col rounded-md bg-gray-0"
+      >
+        <SingleInvoice
+          v-for="invoice in invoices"
+          :key="invoice.id"
+          :invoice="invoice"
+        />
       </div>
     </div>
   </div>
--- src/stores/invoices.ts
+++ src/stores/invoices.ts
@@ -1,18 +1,36 @@
-import { ref } from 'vue';
-import { defineStore } from 'pinia';
+import { ref } from "vue";
+import { defineStore } from "pinia";
 
-export const useInvoicesStore = defineStore('invoices', () => {
-  const currentFilter = ref('All');
+// export const useInvoicesStore = defineStore("invoices", () => {
+//   const currentFilter = ref("All");
 
-  const sortByRaw = ref('paymentDue-asc');
+//   const sortByRaw = ref("paymentDue-asc");
 
-  const setSortByRaw = (val: string) => {
-    sortByRaw.value = val;
-  };
+//   const setSortByRaw = (val: string) => {
+//     sortByRaw.value = val;
+//   };
 
-  function setFilter(val: string) {
-    currentFilter.value = val;
-  }
+//   function setFilter(val: string) {
+//     currentFilter.value = val;
+//   }
 
-  return { currentFilter, setFilter, sortByRaw, setSortByRaw };
+//   return { currentFilter, setFilter, sortByRaw, setSortByRaw };
+// });
+
+export const useInvoicesStore = defineStore("invoices", {
+  state: () => ({
+    currentFilter: "All",
+
+    sortByRaw: "paymentDue-asc",
+  }),
+
+  actions: {
+    setFilter(val: string) {
+      this.currentFilter = val;
+    },
+
+    setSortByRaw(val: string) {
+      this.sortByRaw = val;
+    },
+  },
 });
